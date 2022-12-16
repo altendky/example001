8a76e61a32c77a1ce00699d06dc79d00e4ae9f7d
Kyle Altendorf <sda@fstab.net>
2022-12-16 10:38:44 -0500

    more examples

--- go.sh
+++ go.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+set -vx
+
 python3 -m venv venv
-venv/bin/pip install coverage==6.5.0
+venv/bin/python --version --version
+#venv/bin/pip install coverage==6.5.0
+venv/bin/pip install 'coverage @ git+https://github.com/nedbat/coveragepy@d20c1d0329b585920cf8b1a36af5d2d9c4bba155'
 
-venv/bin/coverage erase
-venv/bin/coverage run -m tests
-venv/bin/coverage xml -o coverage.xml
-cat coverage.xml
+venv/bin/coverage debug sys
+venv/bin/pip freeze --all
 
-venv/bin/coverage erase
-venv/bin/coverage run --rcfile maybe_coveragerc -m tests
-venv/bin/coverage xml --rcfile maybe_coveragerc -o coverage.maybe.xml
-cat coverage.maybe.xml
+for label in no_source source source_pkgs
+do
+  venv/bin/coverage erase
+  venv/bin/coverage run --rcfile coveragerc.${label} -m tests
+  venv/bin/coverage xml --rcfile coveragerc.${label} -o coverage.${label}.xml
+  cat coverage.${label}.xml
+done
+
+#venv/bin/coverage erase
+#venv/bin/coverage run -m tests
+#venv/bin/coverage xml -o coverage.noconfig.xml
+#cat coverage.noconfig.xml
+#
+#venv/bin/coverage erase
+#venv/bin/coverage run --rcfile coveragerc.source -m tests
+#venv/bin/coverage xml --rcfile coveragerc.source -o coverage.source.xml
+#cat coverage.source.xml
+#
+#venv/bin/coverage erase
+#venv/bin/coverage run --rcfile coveragerc.source_pkgs -m tests
+#venv/bin/coverage xml --rcfile coveragerc.source_pkgs -o coverage.source_pkgs.xml
+#cat coverage.source_pkgs.xml
